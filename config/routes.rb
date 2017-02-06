46a3ccfc3cdc90c087453eeab8823a56d52c36dc
yonas samuel <yonastlolo@yahoo.com>
2017-02-06 15:48:55 -0500

    writing the routes, controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  
+
+  resources :products do
+    resources :reviews, only: [:show, :create, :destroy]
+  end
+
   resources :users, only: [:new, :create]
   resources :products
   resources :sessions, only: [:new, :creat, :destroy]
