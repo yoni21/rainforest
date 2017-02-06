b2218ad4230bbcfa3ff9fb3c8a762faf6604fae5
yonas samuel <yonastlolo@yahoo.com>
2017-02-06 15:01:04 -0500

    Creating a user model, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  # get 'products/index'
-  #
-  # get 'products/show'
-  #
-  # get 'products/new'
-  #
-  # get 'products/edit'
- resources :products
+  resources :users, only: [:new, :create]
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
