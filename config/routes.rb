868526df37cca8877feeb9a94b9deec1d43a2dd2
yonas samuel <yonastlolo@yahoo.com>
2017-02-07 06:09:17 -0500

    Home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'products/index'
 
   resources :products do
   resources :reviews, only: [:show, :create, :destroy]
@@ -8,4 +9,5 @@ Rails.application.routes.draw do
   resources :products
   resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root 'products#index'
 end
